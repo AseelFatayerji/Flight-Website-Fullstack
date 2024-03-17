80c41b0ca355005f6909df8f40cd03f640f4e3ef
hamada <hamadahandoush9991@gmail.com>
2024-03-17 20:45:46 +0200

    added login page with style

--- Backend/signup.php
+++ Backend/signup.php
@@ -1,27 +1,27 @@
 <?php
-include('connection.php');
+include('./connection.php');
 
 
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+$user_name = $_POST['user_name'];
+$user_email = $_POST['user_email'];
+$user_password = $_POST['user_password'];
 
 $check_email = $mysqli->prepare('select user_email from users where user_email=?');
-$check_email->bind_param('s', $email);
+$check_email->bind_param('s', $user_email);
 $check_email->execute();
 $check_email->store_result();
 $email_exists = $check_email->num_rows();
 
 
 if ($email_exists == 0) {
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $query = $mysqli->prepare('insert into users(user_name,password,user_email) values(?,?,?);');
-    $query->bind_param('sss', $username, $hashed_password, $email);
+    $hashed_password = password_hash($user_password, PASSWORD_BCRYPT);
+    $query = $mysqli->prepare('insert into users(user_name,user_email,user_password) values(?,?,?);');
+    $query->bind_param('sss', $user_name,$user_email,$hashed_password);
     $query->execute();
     $response['status'] = "success";
-    $response['message'] = "user $username was created successfully";
+    $response['message'] = "user $user_name was created successfully";
 } else {
     $response["status"] = "user already exists";
-    $response["message"] = "user $username wasn't created";
+    $response["message"] = "user $user_name wasn't created";
 }
 echo json_encode($response);
--- Frontend/Scripts/signup.js
+++ Frontend/Scripts/signup.js
@@ -1,41 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const signupForm = document.getElementById("signupForm");
-  const form = document.getElementById("form");
   const messageDiv = document.getElementById("message");
-  const toLogin = document.getElementById("to-login");
-  const formLogin = document.getElementById("form-login");
-  const toSign = document.getElementById("to-sign");
-  const loginForm = document.getElementById("loginform");
+  //   const toLogin = document.getElementById("to-login");
 
   const signUp = async () => {
     const formData = new FormData(signupForm);
 
     try {
-      const response = await fetch("http://localhost/project22/index.php", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        messageDiv.textContent = data.message;
-      } else {
-        const errorMessage = await response.text();
-        messageDiv.textContent = `Error: ${errorMessage}`;
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      messageDiv.textContent = "An error occurred. Please try again later.";
-    }
-  };
-  const signIn = async () => {
-    const formData = new FormData(loginForm);
-
-    try {
-      const response = await fetch("http://localhost/project22/login.php", {
-        method: "POST",
-        body: formData,
-      });
+      const response = await fetch(
+        "http://localhost/flights/Flight-Website-Fullsatack/Backend/signup.php",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       if (response.ok) {
         const data = await response.json();
@@ -55,18 +33,4 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     signUp();
   });
-
-  // for switching
-  const toSigns = () => {
-    form.classList.remove("hide");
-    formLogin.classList.add("hide");
-  };
-
-  const toLogins = () => {
-    form.classList.add("hide");
-    formLogin.classList.remove("hide");
-  };
-
-  toLogin.addEventListener("click", toLogins);
-  toSign.addEventListener("click", toSigns);
 });
