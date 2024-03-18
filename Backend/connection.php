244536bd56055a7be6aefc4a670f9194e89f9fc8
hamada <hamadahandoush9991@gmail.com>
2024-03-18 13:58:49 +0200

    solved a bug

--- Backend/connection.php
+++ Backend/connection.php
@@ -1,11 +1,12 @@
 <?php
+
     $host = "localhost";
     $user = "root";
     $pass = "";
     $db_name = "flightdb";
-    $port = "3308";
+   
 
-    $mysqli = new mysqli($host, $user, $pass, $db_name,$port);
+    $mysqli = new mysqli($host, $user, $pass, $db_name);
     if ($mysqli->connect_error) {
         die("". $mysqli->connect_error);
     }
--- Frontend/Scripts/login.js
+++ Frontend/Scripts/login.js
@@ -1,33 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
   const messageDiv = document.getElementById("message");
 
-  loginForm.addEventListener("submit", async (event) => {
+  loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const formData = new FormData(loginForm);
 
-    try {
-      const response = await fetch(
-        "http://localhost/flights/Fligh-Website-Fullsatack/login.php",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        messageDiv.textContent = data.message;
-        // Optionally redirect to another page on successful login
-        // window.location.href = '/dashboard.html';
-      } else {
-        const errorMessage = await response.text();
-        messageDiv.textContent = `Error: ${errorMessage}`;
+    fetch(
+      "http://localhost:8000/flights/flight-website-fullsatack/backend/login.php",
+      {
+        method: "POST",
+        body: formData,
       }
-    } catch (error) {
-      console.error("Error:", error);
-      messageDiv.textContent = "An error occurred. Please try again later.";
-    }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Check if authentication was successful
+        if (data.success) {
+          // Redirect to dashboard or another page on successful login
+          window.location.href = "/dashboard.html";
+        } else {
+          // Display error message
+          messageDiv.textContent = "Invalid email or password";
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+        // Display error message
+        messageDiv.textContent = "An error occurred. Please try again later.";
+      });
   });
 });
