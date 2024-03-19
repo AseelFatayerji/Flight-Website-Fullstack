6defba5053e39d4dbdcbd231507e8070a1ed1b42
hamada <hamadahandoush9991@gmail.com>
2024-03-19 13:46:45 +0200

    add some changes to user rprofile

--- Frontend/Scripts/profile.js
+++ Frontend/Scripts/profile.js
@@ -1,14 +1,16 @@
 const editBox = document.getElementById("editBox");
+const editUser = document.getElementById("editUser");
 const edit = document.getElementById("edit");
+const closing = document.getElementById("close");
 const overlay = document.getElementById("overlay");
 const userId = JSON.parse(localStorage.getItem("userId"));
 const fetchUserProfile = (userId) => {
   fetch(
-    `http://localhost/flight/flight-website-fullstack/flight-website-fullstack/backend/profile.php?id=${userId}`
+    `http://localhost/flight/flight-website-fullstack/flight-website-fullstack/backend/profile.php?id=2`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.user);
+      console.log(data);
       console.log("ahmad");
       if (data.status === "success") {
         displayUserProfile(data.user);
@@ -25,11 +27,12 @@ const openBox = () => {
   editBox.classList.add("open");
   overlay.classList.add("active");
 };
-function displayUserProfile(user) {
-  document.getElementById("username").textContent = user.username;
-  document.getElementById("email").textContent = user.email;
-}
+const closeBox = () => {
+  editBox.classList.remove("open");
+  overlay.classList.remove("active");
+};
+// function displayUserProfile(user) {}
 
-fetchUserProfile(userId);
-
+closing.addEventListener("click", closeBox);
 edit.addEventListener("click", openBox);
+fetchUserProfile(userId);
