4518013b3e75c8ffefd9f97a880fba45e52a3ba8
hamada <hamadahandoush9991@gmail.com>
2024-03-19 02:38:56 +0200

    added the edit box

--- Frontend/Scripts/profile.js
+++ Frontend/Scripts/profile.js
@@ -1,28 +1,35 @@
-const userID = localStorage.getItem("userID");
+const editBox = document.getElementById("editBox");
+const edit = document.getElementById("edit");
+const overlay = document.getElementById("overlay");
+const userId = JSON.parse(localStorage.getItem("userId"));
+const fetchUserProfile = (userId) => {
+  fetch(
+    `http://localhost/flight/flight-website-fullstack/flight-website-fullstack/backend/profile.php?id=${userId}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.user);
+      console.log("ahmad");
+      if (data.status === "success") {
+        displayUserProfile(data.user);
+      } else {
+        alert(data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
 
-console.log(userID);
+const openBox = () => {
+  editBox.classList.add("open");
+  overlay.classList.add("active");
+};
+function displayUserProfile(user) {
+  document.getElementById("username").textContent = user.username;
+  document.getElementById("email").textContent = user.email;
+}
 
-// const fetchUserProfile = (userID) => {
-//   fetch(
-//     `http://localhost/flight/flight-website-fullstack/flight-website-fullstack/backend/profile.php?id=${userID}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data.user);
-//       if (data.status === "success") {
-//         displayUserProfile(data.user);
-//       } else {
-//         alert(data.message);
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// };
+fetchUserProfile(userId);
 
-// function displayUserProfile(user) {
-//   document.getElementById("username").textContent = user.username;
-//   document.getElementById("email").textContent = user.email;
-// }
-
-// fetchUserProfile(userID);
+edit.addEventListener("click", openBox);
