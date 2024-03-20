6446e9030092cfd4ae0428b40284a2a1bfecbbda
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-20 12:30:43 +0200

    completed the basic homepage functionality

--- Backend/getGoodReviews.php
+++ Backend/getGoodReviews.php
@@ -2,21 +2,26 @@
 
 include 'connection.php';
 
-$query = $mysqli->prepare("SELECT * FROM reviews WHERE rating > 3");
+$query = $mysqli->prepare("SELECT r.*, u.user_name FROM reviews as r join users as u on r.user_id = u.user_id  WHERE rating > 3");
 $query->execute();
 $result = $query->store_result();
 
 $goodReviews = array();
-$query->bind_result($id,$user,$flight, $rating, $comment);
+$query->bind_result($id,$user,$flight, $rating, $comment,$approved,$name);
 while($query->fetch()){
     $review = array(
         'id' => $id,
         'user' => $user,
         'flight' => $flight,
         'rating' => $rating,
-        'comment' => $comment
+        'comment' => $comment,
+        'approved' =>$approved,
+        'name' =>$name,
     );
     array_push($goodReviews, $review);
     $response['status'] = "success";
     $response['goodReviews'] = $goodReviews;
 }
+
+
+echo json_encode($goodReviews);
--- Frontend/Scripts/home.js
+++ Frontend/Scripts/home.js
@@ -1,7 +1,8 @@
-const upcomingFlights = document.getElementById("upcoming");
+const upcomingFlights = document.getElementById("upcoming-cards");
 const flightCards = document.getElementById("flight-cards");
+const reviews = document.getElementById("reviews");
 
-const getUpcomingFlights = async () => {
+const getAllFlights = async () => {
   const response = await fetch(
     "http://localhost:81/flight-website-fullstack/Backend/getAllFlights.php",
     {
@@ -13,6 +14,44 @@ const getUpcomingFlights = async () => {
       console.log(data);
       data.forEach((element) => {
         createFlightCard(element);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getUpcomingFlights = async () => {
+  const response = await fetch(
+    "http://localhost:81/flight-website-fullstack/Backend/getUpcomingFlights.php",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        createUpcomingFlightCard(element);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getGoodReviews = async () => {
+  const response = await fetch(
+    "http://localhost:81/flight-website-fullstack/Backend/getGoodReviews.php",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        createGoodReviewCard(element);
       });
     })
     .catch((error) => {
@@ -39,4 +78,39 @@ const createFlightCard = (flight) => {
   flightCards.appendChild(card);
 };
 
+const createUpcomingFlightCard = (flight) => {
+  const card = document.createElement("div");
+  card.classList.add("flight-card", "txt-white", "bg-secondary");
+  card.innerHTML = `<img
+    src="${flight.image}"
+    alt=""
+  />
+  <div class="flex center column">
+    <p>${flight.id}</p>
+    <ul class="flex space-evenly">
+      <li>${flight.destination}</li>
+      <li>${flight.departure}</li>
+      <li>${flight.return}</li>
+    </ul>
+  </div>`;
+
+  upcomingFlights.appendChild(card);
+};
+
+
+const createGoodReviewCard = (review) => {
+  const card = document.createElement("div");
+  card.classList.add("review-card", "txt-white", "bg-primary","flex","center","column");
+  card.innerHTML = `<ul class="flex space-evenly review-text">
+  <li>${review.rating}</li>
+  <li>flight ${review.flight}</li>
+  <li>${review.name}</li>
+</ul>
+<p class="review-text">${review.comment}</p>`;
+
+  reviews.appendChild(card);
+};
+
 getUpcomingFlights();
+getAllFlights();
+getGoodReviews();
