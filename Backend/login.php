cf5ba33ff0fb9b64ecf71c086f84eea78b28f05a
hamada <hamadahandoush9991@gmail.com>
2024-03-21 00:14:12 +0200

    add

--- Backend/login.php
+++ Backend/login.php
@@ -1,8 +1,9 @@
 <?php
 include('connection.php');
-
 $email = $_POST['user_email'];
 $password = $_POST['user_password'];
+
+
 
 $query = $mysqli->prepare('select user_id,user_email,user_password,user_name,is_admin
 from users 
@@ -18,6 +19,9 @@ if ($num_rows == 0) {
     $response['status'] = "user not found";
 } else {
     if (password_verify($password, $hashed_password)) {
+        
+
+       
         $response['status'] = "logged in";
         $response['user_id'] = $id;
         $response['name'] = $name;
