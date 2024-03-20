88de24a51959c93a43344bc016c347e7066dd60b
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-20 22:00:01 +0200

    craeted the flight search page

---
+++ Frontend/Scripts/allflights.js
@@ -0,0 +1,7 @@
+const filter_btn = document.getElementById('filter-btn');
+const search_input = document.getElementById('search-input');
+const search_btn = document.getElementById('search-btn');
+const passengers_less_than = document.getElementById('passengers-less-than');
+const departure_after = document.getElementById('departure-after');
+const return_before = document.getElementById('return-before');
+const display_flights = document.getElementById('display-flights');
