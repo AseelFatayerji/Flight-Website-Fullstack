c4dab4abc1cea462041b91a1a5fb7ac7a9199e54
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-21 01:37:26 +0200

    finished diplaying flight info logic

--- Frontend/Scripts/flight.js
+++ Frontend/Scripts/flight.js
@@ -9,9 +9,7 @@ const departure_date = document.getElementById("departure");
 const return_date = document.getElementById("return");
 const price = document.getElementById("price");
 const flight_img = document.getElementById("flight-img");
-
-
-
+const booking = document.getElementById("booking");
 
 const URLParams = (id) => {
   const urlParams = new URLSearchParams(window.location.search);
@@ -19,18 +17,48 @@ const URLParams = (id) => {
 };
 
 const flight_id = URLParams("id");
-const user_id= localStorage.getItem("userId");
+const user_id = localStorage.getItem("userId");
 
+console.log(flight_id);
 const getFlight = async () => {
-    const response = await fetch(`http://localhost:81/flight-website-fullstack/Backend/getAllFlights.php?id=${flight_id}`,{
-        method:"GET"
-    }).then(response=>response.json())
-    .then(data=>{
-        console.log(data);
-        createFlightCard(data);
-    }).catch(error=>{
-        console.log(error);
+  const response = await fetch(
+    `http://localhost:81/flight-website-fullstack/Backend/getFlight.php?id=${flight_id}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      flight_img.src = data.flight.image;
+      destination.innerHTML +=" "+ data.flight.destination;
+      departure_date.innerText +=" "+ data.flight.departure;
+      return_date.innerText +=" "+ data.flight.return;
+      price.innerText +=" "+ data.flight.price;
     })
-    
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+const getBookings = async () => {
+  const response = await fetch(
+    `http://localhost:81/flight-website-fullstack/Backend/DisplayBookings`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((element) => {
+        if (element.flight_id == flight_id && element.user_id == user_id) {
+        }
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+getFlight();
