819270dcdd51c89aed25ca789fb04b52839cd0e6
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-21 02:20:54 +0200

    created the upload review and add booking logic

--- Frontend/Scripts/flight.js
+++ Frontend/Scripts/flight.js
@@ -1,3 +1,5 @@
+const { data } = require("jquery");
+
 const one = document.getElementById("1");
 const two = document.getElementById("2");
 const three = document.getElementById("3");
@@ -10,6 +12,13 @@ const return_date = document.getElementById("return");
 const price = document.getElementById("price");
 const flight_img = document.getElementById("flight-img");
 const booking = document.getElementById("booking");
+const upload_btn = document.getElementById("upload-btn")
+const book_btn = document.getElementById("book-btn")
+let stars = document.querySelectorAll("star");
+
+
+let rating = 0;
+
 
 const URLParams = (id) => {
   const urlParams = new URLSearchParams(window.location.search);
@@ -19,7 +28,7 @@ const URLParams = (id) => {
 const flight_id = URLParams("id");
 const user_id = localStorage.getItem("userId");
 
-console.log(flight_id);
+
 const getFlight = async () => {
   const response = await fetch(
     `http://localhost:81/flight-website-fullstack/Backend/getFlight.php?id=${flight_id}`,
@@ -41,24 +50,70 @@ const getFlight = async () => {
     });
 };
 
-const getBookings = async () => {
+const bookFlight = async () => {
+    const form_data = new FormData();
+    form-data.append("flight_id", flight_id);
+    form-data.append("user_id", user_id);
+const response = await fetch(`http://localhost:81/flight-website-fullstack/Backend/getFlight.php`, {
+method: "POST",
+body: form_data,
+})
+
+}
+
+
+
+const addReview = async () => {
+  const form_data = new FormData();
+  form_data.append("flight_id", flight_id);
+  form_data.append("user_id", user_id);
+  form_data.append("rating", rating);
+  form_data.append("review", review);
   const response = await fetch(
-    `http://localhost:81/flight-website-fullstack/Backend/DisplayBookings`,
+    `http://localhost:81/flight-website-fullstack/Backend/addReview.php`,
     {
-      method: "GET",
+      method: "POST",
+      body: form_data,
     }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      data.forEach((element) => {
-        if (element.flight_id == flight_id && element.user_id == user_id) {
-        }
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+  );
+}
+
+
+
+one.addEventListener("click", () => {
+  one.classList.toggle("fa-regular")
+  one.classList.toggle("fa-solid")
+})
+
+two.addEventListener("click", () => {
+  two.classList.toggle("fa-regular")
+  two.classList.toggle("fa-solid")
+})
+
+three.addEventListener("click", () => {
+  three.classList.toggle("fa-regular")
+  three.classList.toggle("fa-solid")
+})
+
+four.addEventListener("click", () => {
+  four.classList.toggle("fa-regular")
+  four.classList.toggle("fa-solid")
+})
+
+five.addEventListener("click", () => {
+  five.classList.remove("fa-regular")
+  five.classList.add("fa-solid")
+})
+
+
+
+
+book_btn.addEventListener("click", () => {
+  bookFlight();
+})
+
+upload_btn.addEventListener("click", () => {
+  addReview();
+})
 
 getFlight();
